579067945a0fda66664a2cfc8924a021e153d94e
peerapat-thongtae <peera.thongtae@gmail.com>
2021-07-06 20:19:12 +0700

    port env

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
